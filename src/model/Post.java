f0e01da485c0b6b888d16ad5f97aca59bd4c8b95
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-17 11:43:09 +0200

    Add: added variables, getters and seters in the Post class

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,49 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Post {
+	//1. mainigie
+	private String msg;
+	private LocalDateTime datetime = LocalDateTime.now();
+	private int countOfLikes = 0;//TODO parveidot uz sarakstu ar registretiem lietotjamiem, kas bus atzimejusi patik
+	//2.getter
+	public String getMsg() {
+		return msg;
+	}
+	
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+	
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+	//3.setter
+	public void setMsg(String inputMsg) {
+		if((inputMsg != null) && (!inputMsg.isEmpty()) && (inputMsg.length() < 1000))
+		{
+			msg = inputMsg;
+		}
+		else
+		{
+			msg = "Unknown post";
+		}
+	}
+
+	public void setDatetime() {
+		datetime = LocalDateTime.now();
+	}
+
+	public void incrementCountOfLikes() {
+		countOfLikes++;
+	}
+	
+	
+	
+
+	//4 abi konstruktori
+	//5.toString
+	//6.citas funkcijas (pec nepieciesamibas)
 
 }
